6850c4e8c70314f981724335f11aa901ae18ab8c
AbrRahman <abrahman.est@gamil.com>
2025-10-06 19:20:47 +0600

    push college page ui

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home";
+import College from "../pages/college/College";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "", element: <Home /> }],
+    children: [
+      { path: "", element: <Home /> },
+      { path: "/college", element: <College /> },
+    ],
   },
 ]);
